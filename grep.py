30ab000aab9dc52f2f785e2440c6b71cc0efecd1
Handi <handi@hashmicro.id>
2022-10-13 10:26:04 +0700

    finish logic whole word and insensitive option

--- grep.py
+++ grep.py
@@ -1,80 +1,102 @@
-from genericpath import isdir
 import os
 import sys
 import re
 
-def get_valid_arguments():
-  args = sys.argv[1:]  # ambil argumen setelah nama file program
 
-  # argumen yang valid diberikan minimal 2, maksimal 3
-  if len(args) > 3 or len(args) < 2:
-    return
-  
-  # jika argumen ada 3 maka ia valid iff nilainya -i atau -w
-  if len(args) == 3:
-    if len(args[0]) == 2:
-      if re.search('^-', args[0]):
-        if args[0][1] == 'w' or args[0][1] == 'i':
-          return args
-    return
-  
-  # jumlah argumen 2
-  return args
-
-def print_line(path, line, sentence):
-  print("{:40s} line {:<3d} {:40s}".format(path, line, sentence))
-
-def scan_file(file, pattern, direktori):
-  line_idx = 0
-  for line in file:
-    line_idx += 1
-    if re.search(pattern, line.strip()):
-      print_line(direktori, line_idx, line.strip())
-
-def handle_with_option(option, direktori, pattern, is_file):
-  pass
-
-def handle_without_option(direktori, pattern, is_file):
-  if is_file:
-    file = open(direktori, "r")
-    scan_file(file, pattern, direktori)
-    file.close()
-    return
-  
-  if direktori[-1:] != "\\":
-    direktori = direktori + "\\"
-  
-  for dir in os.listdir(direktori):
-    new_dir = direktori+dir
-    if os.path.isfile(new_dir):
-      handle_without_option(new_dir, pattern, True)
-    elif os.path.isdir(new_dir):
-      handle_without_option(new_dir, pattern, False)
-
-def main():
-  valid_args = get_valid_arguments()
-
-  if valid_args is None: 
-    print("Argumen program tidak benar.")
-    return
-
-  is_file = False
-  option = ""
-
-  direktori = valid_args[1] if len(valid_args) == 2 else valid_args[2]
-  if os.path.exists(direktori):
-    if os.path.isfile(direktori):
-      is_file = True
-  else:
-    print(f"Path {direktori} tidak ditemukan.")
-    return
-
-  if len(valid_args) == 3:
-    option = valid_args[0]
-    handle_with_option(option, direktori, valid_args[1], is_file)
-  else:
-    handle_without_option(direktori, valid_args[0], is_file)
+def list_files(folder):
+    """Mencari sekumpulan file ditentukan dari folder"""
+    locations = []
+    for (root, dirs, files) in os.walk(folder, topdown=True):
+        for arr in files:
+            if root[-1:] != "\\":
+                locations.append("{}\{}".format(root, arr))
+            else:
+                locations.append("{}{}".format(root, arr))
+    return locations
 
 
-if __name__ == '__main__':
-  main()
+def mode_default(pattern, location):
+    """Mode tanpa argumen options"""
+    files = list_files(location)
+    if (len(files) > 0):
+        for i in range(len(files)):
+            sentences = scan_file(files[i])
+            for count in range(len(sentences)):
+                # hanya mengecek konten di dalam kalimat
+                if(pattern in sentences[count]):
+                    print_line(files[i], count + 1, sentences[count])
+    else:
+        sentences = scan_file(location)
+        for count in range(len(sentences)):
+            # hanya mengecek konten di dalam kalimat
+            if(pattern in sentences[count]):
+                print_line(location, count + 1, sentences[count])
+
+# TODO: edit logic pattern match string
+def mode_whole_word(pattern, location):
+    """Mode argumen menggunakan -w"""
+    files = list_files(location)
+    if (len(files) > 0):
+        for i in range(len(files)):
+            sentences = scan_file(files[i])
+            for count in range(len(sentences)):
+                if(re.search(rf"\b{pattern}\b", sentences[count])):
+                    print_line(files[i], count + 1, sentences[count])
+    else:
+        sentences = scan_file(location)
+        for count in range(len(sentences)):
+            if(re.search(rf"\b{pattern}\b", sentences[count])):
+                print_line(location, count + 1, sentences[count])
+
+# TODO: edit logic pattern match string
+def mode_insensitive(pattern, location):
+    """Mode argumen menggunakan -i"""
+    files = list_files(location)
+    if (len(files) > 0):
+        for i in range(len(files)):
+            sentences = scan_file(files[i])
+            for count in range(len(sentences)):
+                if(re.search(f"{pattern.lower()}", sentences[count].lower())):
+                    print_line(files[i], count + 1, sentences[count])
+    else:
+        sentences = scan_file(location)
+        for count in range(len(sentences)):
+            if(re.search(f"{pattern.lower()}", sentences[count].lower())):
+                print_line(location, count + 1, sentences[count])
+
+
+def scan_file(location):
+    """Melakukan pemindaian isi dari file"""
+    try:
+        with open(location, "r") as file:
+            output = []
+            for sentence in file:
+                output.append(sentence.strip())
+            return output
+    except FileNotFoundError:
+        print(f"Path {location} tidak ditemukan")
+
+
+def print_line(location, line, sentence):
+    """Mencetak format string di terminal"""
+    print(f"{location:<40} line {line:<3} {sentence[0:40]}")
+
+
+if __name__ == "__main__":
+    # input berdasarkan argumen program berjumlah 3
+    if (2 < len(sys.argv) <= 4):
+        option = sys.argv[1]
+        pattern = sys.argv[len(sys.argv) - 2]
+        location = sys.argv[len(sys.argv) - 1]
+        # case: option tidak digunakan
+        if (option == pattern):
+            # TODO: kalau pattern adalah option
+            mode_default(pattern, location)
+        elif (option == '-w'):
+            mode_whole_word(pattern, location)
+        elif (option == '-i'):
+            mode_insensitive(pattern, location)
+        else:
+            raise ValueError("Argumen program tidak benar.")
+    else:
+        raise ValueError("Argumen program tidak benar.")
